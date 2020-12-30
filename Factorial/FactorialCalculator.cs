a04b0add78b368d8b494a764a93022da32f29334
heather dougherty <heatherdougherty1234@me.com>
2020-12-29 20:22:54 -0500

    implemented factorialmethod

--- Factorial/FactorialCalculator.cs
+++ Factorial/FactorialCalculator.cs
@@ -2,14 +2,31 @@
 
 namespace Factorial
 {
-	public class FactorialCalculator
-	{
-		/// <summary>
-		/// returns the factorial of the value parameter.
-		/// </summary>
-		public int Factorial(int value)
-		{
-			throw new NotImplementedException();
-		}
-	}
+  public class FactorialCalculator
+  {
+    /// <summary>
+    /// returns the factorial of the value parameter.
+    /// </summary>
+    public int Factorial(int value)
+    {
+      if (value < 0)
+      {
+        throw new ArgumentOutOfRangeException();
+      }
+
+      if (value == 0)
+      {
+        return 0;
+      }
+
+      int answer = value;
+
+      for (int i = value - 1; i > 1; i--)
+      {
+        answer *= i;
+      }
+
+      return answer;
+    }
+  }
 }
